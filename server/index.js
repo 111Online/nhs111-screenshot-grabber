dcd91ab9754da6f52f413cfb300884f2020d771e
Shane Hudson <shane@shanehudson.net>
2017-12-20 13:51:28 +0000

    Change host from localhost to all.

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ import api from './api'
 
 const bodyParser = require('body-parser')
 const app = express()
-const host = process.env.HOST || '127.0.0.1'
+const host = process.env.HOST || '0.0.0.0'
 const port = process.env.PORT || 3000
 
 app.set('port', port)
