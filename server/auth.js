ebaa0b842e2c17aa45e505b6bc8e892f71b85749
Shane Hudson <shane@shanehudson.net>
2019-09-25 16:35:25 +0100

    Renaming env vars so no conflict

--- server/auth.js
+++ server/auth.js
@@ -2,12 +2,11 @@ const auth = require('basic-auth')
 
 module.exports = function (req, res, next) {
   const credentials = auth(req)
-
-  if (credentials && (!process.env.USERNAME || !process.env.PASSWORD)) {
+  if (credentials && (!process.env.AUTH_USER || !process.env.AUTH_PASS)) {
     // No user/pass set in environmental variables, so proceed
     return next()
   }
-  else if (!credentials || credentials.name !== process.env.USERNAME || credentials.pass !== process.env.PASSWORD) {
+  else if (!credentials || credentials.name !== process.env.AUTH_USER || credentials.pass !== process.env.AUTH_PASS) {
     res.statusCode = 401
     res.setHeader('WWW-Authenticate', 'Basic realm="nhs-111"')
     res.end('Access denied')
